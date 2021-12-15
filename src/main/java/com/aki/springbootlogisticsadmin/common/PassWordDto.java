c7743afda04aa76ba6f42614be7e69d2e1af675e
Casuor <1833708553@qq.com>
2021-12-15 10:13:40 +0800

    springboot step 12.15

---
+++ src/main/java/com/aki/springbootlogisticsadmin/common/PassWordDto.java
@@ -0,0 +1,14 @@
+package com.aki.springbootlogisticsadmin.common;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+public class PassWordDto implements Serializable {
+    @NotBlank(message = "新密码不能为空")
+    private String password;
+    @NotBlank(message = "旧密码不能为空")
+    private String currentPassword;
+}
